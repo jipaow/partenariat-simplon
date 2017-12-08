7f04b70ec63eb2f3d6b9fca54642c53a0e9c1eb3
jipaow <32737311+jipaow@users.noreply.github.com>
2017-12-08 10:27:46 +0100

    coorection erreur de code

--- partenariat-simplon.php
+++ partenariat-simplon.php
@@ -1,20 +1,20 @@
 
 <?php
 /*
-Plugin Name:  WordPress.org Plugin
+Plugin Name:  simplon
 Plugin URI:   https://developer.wordpress.org/plugins/the-basics/
 Description:  Basic WordPress Plugin Header Comment
 Version:      20160911
-Author:       WordPress.org
+Author:       JipPres
 Author URI:   https://developer.wordpress.org/
-License:      GPL2
+License:      LPS2
 License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain:  wporg
 Domain Path:  /languages
 */
 function Shortcode() {
 	return '<p>La publication de cet article est possible grâce à mon super partenaire 
-<a href="https://simplon.co>simplon.co</a>
+<a href="https://simplon.co">simplon.co</a>
  - une entreprise de 
 l’économie sociale et solidaire et un
 réseau de « fabriques » (écoles) qui propose 
